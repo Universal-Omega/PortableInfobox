af6b720565f82f8a29607b51c6cb70da1f2623ab
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-06-03 17:52:57 -0600

    Update NodeUnimplemented.php

--- includes/services/Parser/Nodes/NodeUnimplemented.php
+++ includes/services/Parser/Nodes/NodeUnimplemented.php
@@ -1,13 +1,14 @@
 <?php
+
 namespace PortableInfobox\Parser\Nodes;
 
-class NodeUnimplemented extends Node {
+use Exception;
 
+class NodeUnimplemented extends Node {
 	public function getData() {
 		throw new UnimplementedNodeException( $this->getType() );
 	}
 }
 
 // phpcs:ignore Generic.Files.OneObjectStructurePerFile.MultipleFound
-class UnimplementedNodeException extends \Exception {
-}
+class UnimplementedNodeException extends Exception {}
