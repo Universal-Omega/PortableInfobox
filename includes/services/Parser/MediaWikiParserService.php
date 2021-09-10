b4c7afd957328b8fc09059ac2b2692fe6cb0403d
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-09 19:10:08 -0600

    Update MediaWikiParserService.php

--- includes/services/Parser/MediaWikiParserService.php
+++ includes/services/Parser/MediaWikiParserService.php
@@ -19,14 +19,7 @@ class MediaWikiParserService implements ExternalParser {
 		$this->frame = $frame;
 
 		if ( $wgPortableInfoboxUseTidy && class_exists( '\MediaWiki\Tidy\RemexDriver' ) ) {
-			if ( version_compare( MW_VERSION, '1.36', '>=' ) ) {
-				$this->tidyDriver = MediaWikiServices::getInstance()->getTidy();
-			} else {
-				$this->tidyDriver = \MWTidy::factory( [
-					'driver' => 'RemexHtml',
-					'pwrap' => false
-				] );
-			}
+			$this->tidyDriver = MediaWikiServices::getInstance()->getTidy();
 		}
 	}
 
