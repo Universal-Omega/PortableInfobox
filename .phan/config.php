cc77fee633d6e56235a0f1c9d067783153b672ef
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-12 12:51:17 -0600

    Update config.php

--- .phan/config.php
+++ .phan/config.php
@@ -14,9 +14,7 @@ $cfg['exclude_analysis_directory_list'] = array_merge(
 	]
 );
 
-$cfg['suppress_issue_types'] = array_merge(
-	$cfg['suppress_issue_types'], []
-);
+$cfg['suppress_issue_types'] = [];
 
 $cfg['scalar_implicit_cast'] = true;
 
