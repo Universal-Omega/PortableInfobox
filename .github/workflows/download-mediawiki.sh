8a6f0d5576a6367e758c23114bead44e9fcae83a
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-14 17:37:22 -0600

    Update download-mediawiki.sh

--- .github/workflows/download-mediawiki.sh
+++ .github/workflows/download-mediawiki.sh
@@ -8,5 +8,12 @@ wget https://github.com/wikimedia/mediawiki/archive/"$MW_BRANCH".tar.gz -nv
 tar -zxf "$MW_BRANCH".tar.gz
 mv mediawiki-"$MW_BRANCH" mediawiki
 
-cd mediawiki
+cd mediawiki/skins
+
+wget https://github.com/wikimedia/Vector/archive/"$MW_BRANCH".tar.gz -nv
+tar -zxf "$MW_BRANCH".tar.gz
+mv Vector-"$MW_BRANCH" Vector
+
+cd ..
+
 composer update --prefer-dist --no-progress
