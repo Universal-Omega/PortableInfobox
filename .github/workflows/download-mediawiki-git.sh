1075ff9e5e16e00cbb4e305765a6dcf5c4fa9b0a
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-13 18:01:06 -0600

    Delete download-mediawiki-git.sh

--- .github/workflows/download-mediawiki-git.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-set -ex
-
-MW_BRANCH=$1
-
-git clone https://github.com/wikimedia/mediawiki.git --depth=2 --branch="$MW_BRANCH"
-
-cd mediawiki
-composer update --prefer-dist --no-progress
