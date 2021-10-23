db2697b122bdc9cb0d5db9d3601ea3337835b999
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-10-23 15:13:34 -0600

    Update SpecialPortableInfoboxBuilder.php

--- includes/specials/SpecialPortableInfoboxBuilder.php
+++ includes/specials/SpecialPortableInfoboxBuilder.php
@@ -2,21 +2,18 @@
 
 class SpecialPortableInfoboxBuilder extends SpecialPage {
 	public function __construct() {
-		global $wgNamespaceProtection;
-
 		parent::__construct( 'InfoboxBuilder' );
-		$this->mRestriction = $wgNamespaceProtection[NS_TEMPLATE];
+
+		$this->mRestriction = $this->getConfig()->get( 'NamespaceProtection' )[NS_TEMPLATE];
 	}
 
 	public function execute( $par ) {
-		global $wgNamespaceProtection;
-
 		$out = $this->getOutput();
 
 		$this->setHeaders();
 		$out->enableOOUI();
 
-		if ( $wgNamespaceProtection[NS_TEMPLATE] ) {}
+		if ( $this->getConfig()->get( 'NamespaceProtection' )[NS_TEMPLATE] ) {}
 
 		$out->addModules( [ 'ext.PortableInfobox.styles', 'ext.PortableInfoboxBuilder' ] );
 		$out->addHTML(
