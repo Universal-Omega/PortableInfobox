b4ada23246978dbb42eb8b11a49a125f53e38f07
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-03-11 12:55:46 -0700

    Update NodeFactory.php

--- includes/services/Parser/Nodes/NodeFactory.php
+++ includes/services/Parser/Nodes/NodeFactory.php
@@ -23,7 +23,7 @@ class NodeFactory {
 	 */
 	protected static function getInstance( SimpleXMLElement $xmlNode, array $data ) {
 		$tagType = $xmlNode->getName();
-		$className = 'Node' . mb_convert_case( mb_strtolower( $tagType ), MB_CASE_TITLE );
+		$className = Node::class . mb_convert_case( mb_strtolower( $tagType ), MB_CASE_TITLE );
 		if ( class_exists( $className ) ) {
 			/* @var $instance Node */
 			$instance = new $className( $xmlNode, $data );
