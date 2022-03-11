fcd8c17f123c6767c82810843e0253985a394a55
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-03-11 11:39:12 -0700

    Update NodeFactory.php

--- includes/services/Parser/Nodes/NodeFactory.php
+++ includes/services/Parser/Nodes/NodeFactory.php
@@ -1,7 +1,9 @@
 <?php
+
 namespace PortableInfobox\Parser\Nodes;
 
 use PortableInfobox\Parser\XmlParser;
+use SimpleXMLElement;
 
 class NodeFactory {
 
@@ -9,22 +11,21 @@ class NodeFactory {
 		return self::getInstance( XmlParser::parseXmlString( $text ), $data );
 	}
 
-	public static function newFromSimpleXml( \SimpleXMLElement $xmlNode, array $data = [] ) {
+	public static function newFromSimpleXml( SimpleXMLElement $xmlNode, array $data = [] ) {
 		return self::getInstance( $xmlNode, $data );
 	}
 
 	/**
-	 * @param \SimpleXMLElement $xmlNode
+	 * @param SimpleXMLElement $xmlNode
 	 * @param array $data
 	 *
 	 * @return Node|NodeUnimplemented
 	 */
-	protected static function getInstance( \SimpleXMLElement $xmlNode, array $data ) {
+	protected static function getInstance( SimpleXMLElement $xmlNode, array $data ) {
 		$tagType = $xmlNode->getName();
-		$className = 'PortableInfobox\\Parser\\Nodes\\' . 'Node' .
-					 mb_convert_case( mb_strtolower( $tagType ), MB_CASE_TITLE );
+		$className = 'Node' . mb_convert_case( mb_strtolower( $tagType ), MB_CASE_TITLE );
 		if ( class_exists( $className ) ) {
-			/* @var $instance \PortableInfobox\Parser\Nodes\Node */
+			/* @var $instance Node */
 			$instance = new $className( $xmlNode, $data );
 
 			if ( $instance instanceof Node ) {
