510175ffb2a8a7c8dc1b01f12d10e460928910ba
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-03-11 13:11:10 -0700

    Update PortableInfoboxRenderServiceTest.php

--- tests/phpunit/PortableInfoboxRenderServiceTest.php
+++ tests/phpunit/PortableInfoboxRenderServiceTest.php
@@ -24,6 +24,7 @@ class PortableInfoboxRenderServiceTest extends MediaWikiIntegrationTestCase {
 
 	/**
 	 * @covers ::renderInfobox
+	 * @covers \PortableInfobox\Helpers\PortableInfoboxTemplateEngine
 	 * @param $input
 	 * @param $expectedOutput
 	 * @param $description
