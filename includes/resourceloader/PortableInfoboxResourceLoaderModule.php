78b11f863403f950bf8abebfe4f6e52a9e069466
lens0021 <lorentz0021@gmail.com>
2022-04-17 14:25:02 +0900

    Cast config to bool

--- includes/resourceloader/PortableInfoboxResourceLoaderModule.php
+++ includes/resourceloader/PortableInfoboxResourceLoaderModule.php
@@ -4,7 +4,8 @@ class PortableInfoboxResourceLoaderModule extends ResourceLoaderFileModule {
 	/** @inheritDoc */
 	protected function getLessVars( ResourceLoaderContext $context ) {
 		$lessVars = parent::getLessVars( $context );
-		$lessVars[ 'responsibly-open-collapsed'] = $this->getConfig()->get( 'PortableInfoboxResponsiblyOpenCollapsed' );
+		$lessVars[ 'responsibly-open-collapsed'] =
+			(bool)$this->getConfig()->get( 'PortableInfoboxResponsiblyOpenCollapsed' );
 		return $lessVars;
 	}
 }
