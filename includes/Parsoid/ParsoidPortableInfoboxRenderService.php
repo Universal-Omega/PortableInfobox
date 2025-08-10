7bfdafc44ce1b326a2b22668969f56882a182460
OriginalAuthority <luke@telepedia.net>
2025-08-10 02:47:00 +0100

    Further work on generating infobox template

--- includes/Parsoid/ParsoidPortableInfoboxRenderService.php
+++ includes/Parsoid/ParsoidPortableInfoboxRenderService.php
@@ -3,6 +3,7 @@
 namespace PortableInfobox\Parsoid;
 
 use PortableInfobox\Services\Helpers\InfoboxParamsValidator;
+use PortableInfobox\Services\Helpers\PortableInfoboxTemplateEngine;
 use PortableInfobox\Services\Parser\Nodes\NodeFactory;
 use Wikimedia\Parsoid\Core\Sanitizer;
 use Wikimedia\Parsoid\DOM\Document;
@@ -26,6 +27,8 @@ class ParsoidPortableInfoboxRenderService {
 	private ?InfoboxParamsValidator $infoboxParamsValidator = null;
 
     private array $paramMap = [];
+
+    private ?PortableInfoboxTemplateEngine $templateEngine = null;
 
     public function __construct() {
         // no-op
@@ -67,9 +70,29 @@ class ParsoidPortableInfoboxRenderService {
         $themes = $this->getThemes( $attr );
         $layout = $this->getLayout( $attr );
         $type = $this->getType( $attr );
+        $itemName = $this->getItemName( $attr );
         // @TODO: we need the accentColors and accentColorsText but I've no idea what the heck the controller
         // for the legacy parser is doing to get that and it's confusing me right now so come back to that later.
     
+        // need the other accentcolour etc here when thats figured out.
+        $result = $this->renderInfobox(
+            $data, 
+            implode( ' ', $themes ),
+            $layout, 
+            $type,
+            $itemName
+        );
+
+        // this is a hack around as we need to have a DOMNode to add to the 
+        // parsoid output, rather than a string.
+        $tempDoc = new \DOMDocument( '1.0', 'UTF-8' );
+        $tempDoc->loadHTML( $result, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD );
+        
+        foreach ( $tempDoc->childNodes as $node ) {
+            $importedNode = $doc->importNode( $node, true );
+            $container->appendChild( $importedNode );
+        }
+
     }
 
     public function prepareInfobox(
@@ -112,7 +135,7 @@ class ParsoidPortableInfoboxRenderService {
         $themes = !empty( $themes ) ? $themes : [ self::DEFAULT_THEME_NAME ];
 
         return array_map( static function ( $name ) {
-			return Sanitizer::escapeClass(
+			return Sanitizer::escapeIdForAttribute(
 				self::INFOBOX_THEME_PREFIX . preg_replace( '|\s+|s', '-', $name )
 			);
 		}, $themes );
@@ -149,8 +172,335 @@ class ParsoidPortableInfoboxRenderService {
      * @return string
      */
     private function getType( array $attr ): string {
-		return !empty( $params['type'] ) ? Sanitizer::escapeClass(
-				self::INFOBOX_TYPE_PREFIX . preg_replace( '|\s+|s', '-', $params['type'] )
+		return !empty( $attr['type'] ) ? Sanitizer::escapeIdForAttribute(
+				self::INFOBOX_TYPE_PREFIX . preg_replace( '|\s+|s', '-', $attr['type'] )
 			) : '';
 	}
+
+    /**
+     * Get the name for this infobox
+     * @TODO: needs to go through the sanitizer
+     * @param array $attr 
+     * @return @string
+     */
+    private function getItemName( $attr ) {
+		return !empty( $attr['name'] ) ? $attr['name'] : '';
+	}
+
+    private function renderInfobox(
+        array $data,
+        string $themes,
+        string $layout,
+        string $type,
+        string $itemName
+    ) {
+        $this->templateEngine = new PortableInfoboxTemplateEngine();
+
+        $infoboxHtmlContent = $this->renderChildren( $data );
+
+        if ( !empty( $infoboxHtmlContent ) ) {
+			$output = $this->renderItem( 'wrapper', [
+				'content' => $infoboxHtmlContent,
+				'theme' => $themes,
+				'layout' => $layout,
+				'type' => $type,
+				'item-name' => $itemName
+			] );
+		} else {
+			$output = '';
+		}
+
+		return $output;
+    }
+
+    /**
+     * Render the children of the infobox
+     * @param $children our param -> value data (may be nested and this will be recursively called)
+     * @return string the output
+     */
+    private function renderChildren( array $children ): string {
+        $result = '';
+		foreach ( $children as $child ) {
+			$type = $child['type'];
+			if ( $this->templateEngine->isSupportedType( $type ) ) {
+				$result .= $this->renderItem( $type, $child['data'] );
+			}
+		}
+		return $result;
+    }
+
+    /**
+	 * renders part of infobox
+	 *
+	 * @param string $type
+	 * @param array $data
+	 *
+	 * @return string - HTML
+	 */
+	protected function renderItem( $type, array $data ) {
+		switch ( $type ) {
+			case 'group':
+				$result = $this->renderGroup( $data );
+				break;
+			case 'header':
+				$result = $this->renderHeader( $data );
+				break;
+			case 'media':
+				$result = $this->renderMedia( $data );
+				break;
+			case 'title':
+				$result = $this->renderTitle( $data );
+				break;
+			case 'panel':
+				$result = $this->renderPanel( $data );
+				break;
+			case 'section':
+				$result = '';
+				break;
+			default:
+				$result = $this->renderTemplate( $type, $data );
+				break;
+		}
+
+		return $result;
+	}
+
+    /**
+	 * renders group infobox component
+	 *
+	 * @param array $groupData
+	 *
+	 * @return string - group HTML markup
+	 */
+	protected function renderGroup( array $groupData ) {
+		$cssClasses = [];
+		$groupHTMLContent = '';
+		$children = $groupData['value'];
+		$layout = $groupData['layout'];
+		$collapse = $groupData['collapse'];
+		$rowItems = $groupData['row-items'];
+
+		if ( $rowItems > 0 ) {
+			$items = $this->createSmartGroups( $children, $rowItems );
+			$groupHTMLContent .= $this->renderChildren( $items );
+		} elseif ( $layout === 'horizontal' ) {
+			$groupHTMLContent .= $this->renderItem(
+				'horizontal-group-content',
+				$this->createHorizontalGroupData( $children )
+			);
+		} else {
+			$groupHTMLContent .= $this->renderChildren( $children );
+		}
+
+		if ( $collapse !== null && count( $children ) > 0 && $children[0]['type'] === 'header' ) {
+			$cssClasses[] = 'pi-collapse';
+			$cssClasses[] = 'pi-collapse-' . $collapse;
+		}
+
+		return $this->renderTemplate( 'group', [
+			'content' => $groupHTMLContent,
+			'cssClasses' => implode( ' ', $cssClasses ),
+			'item-name' => $groupData['item-name']
+		] );
+	}
+
+    protected function renderHeader( array $data ) {
+		// $data['inlineStyles'] = $this->inlineStyles;
+
+		return $this->renderTemplate( 'header', $data );
+	}
+
+    protected function renderTemplate( $type, array $data ) {
+		return $this->templateEngine->render( $type, $data );
+	}
+
+    /**
+	 * If image element has invalid thumbnail, doesn't render this element at all.
+	 *
+	 * @param array $data
+	 * @return string
+	 */
+	protected function renderMedia( array $data ) {
+		if ( count( $data ) === 0 || !$data[0] ) {
+			return '';
+		}
+
+		if ( count( $data ) === 1 ) {
+			$data = $data[0];
+			$templateName = 'media';
+		} else {
+			// More than one image means image collection
+			$data = [
+				'images' => $data,
+				'source' => $data[0]['source'],
+				'item-name' => $data[0]['item-name']
+			];
+			$templateName = 'media-collection';
+		}
+
+		return $this->renderTemplate( $templateName, $data );
+	}
+
+    protected function renderTitle( array $data ) {
+		// $data['inlineStyles'] = $this->inlineStyles;
+
+		return $this->renderTemplate( 'title', $data );
+	}
+
+    protected function renderPanel( $data, $type = 'panel' ) {
+		$cssClasses = [];
+		$sections = [];
+		$collapse = $data['collapse'];
+		$header = '';
+		$shouldShowToggles = false;
+
+		foreach ( $data['value'] as $index => $child ) {
+			switch ( $child['type'] ) {
+				case 'header':
+					if ( empty( $header ) ) {
+						$header = $this->renderHeader( $child['data'] );
+					}
+					break;
+				case 'section':
+					$sectionData = $this->getSectionData( $child, $index );
+					// section needs to have content in order to render it
+					if ( !empty( $sectionData['content'] ) ) {
+						$sections[] = $sectionData;
+						if ( !empty( $sectionData['label'] ) ) {
+							$shouldShowToggles = true;
+						}
+					}
+					break;
+				default:
+					// we do not support any other tags than section and header inside panel
+					break;
+			}
+		}
+		if ( $collapse !== null && count( $sections ) > 0 && !empty( $header ) ) {
+			$cssClasses[] = 'pi-collapse';
+			$cssClasses[] = 'pi-collapse-' . $collapse;
+		}
+		if ( count( $sections ) > 0 ) {
+			$sections[0]['active'] = true;
+		} else {
+			// do not render empty panel
+			return '';
+		}
+		if ( !$shouldShowToggles ) {
+			$sections = array_map( static function ( $content ) {
+				$content['active'] = true;
+				return $content;
+			}, $sections );
+		}
+
+		return $this->renderTemplate( $type, [
+			'item-name' => $data['item-name'],
+			'cssClasses' => implode( ' ', $cssClasses ),
+			'header' => $header,
+			'sections' => $sections,
+			'shouldShowToggles' => $shouldShowToggles,
+		] );
+	}
+
+    private function getSectionData( $section, $index ) {
+		$content = $this->renderChildren( $section['data']['value'] );
+		return [
+			'index' => $index,
+			'item-name' => $section['data']['item-name'],
+			'label' => $section['data']['label'],
+			'content' => !empty( $content ) ? $content : null
+		];
+	}
+
+    private function createSmartGroups( array $groupData, $rowCapacity ) {
+		$result = [];
+		$rowSpan = 0;
+		$rowItems = [];
+
+		foreach ( $groupData as $item ) {
+			$data = $item['data'];
+
+			if ( $item['type'] === 'data' && $data['layout'] !== 'default' ) {
+
+				if ( !empty( $rowItems ) && $rowSpan + $data['span'] > $rowCapacity ) {
+					$result[] = $this->createSmartGroupItem( $rowItems, $rowSpan );
+					$rowSpan = 0;
+					$rowItems = [];
+				}
+				$rowSpan += $data['span'];
+				$rowItems[] = $item;
+			} else {
+				// smart wrapping works only for data tags
+				if ( !empty( $rowItems ) ) {
+					$result[] = $this->createSmartGroupItem( $rowItems, $rowSpan );
+					$rowSpan = 0;
+					$rowItems = [];
+				}
+				$result[] = $item;
+			}
+		}
+		if ( !empty( $rowItems ) ) {
+			$result[] = $this->createSmartGroupItem( $rowItems, $rowSpan );
+		}
+
+		return $result;
+	}
+
+    private function createSmartGroupItem( array $rowItems, $rowSpan ) {
+		return [
+			'type' => 'smart-group',
+			'data' => $this->createSmartGroupSections( $rowItems, $rowSpan )
+		];
+	}
+
+    private function createSmartGroupSections( array $rowItems, $capacity ) {
+		return array_reduce( $rowItems, static function ( $result, $item ) use ( $capacity ) {
+			$width = $item['data']['span'] / $capacity * 100;
+			$styles = "width: {$width}%";
+
+			$label = $item['data']['label'] ?? "";
+			if ( !empty( $label ) ) {
+				$result['renderLabels'] = true;
+			}
+			$result['data'][] = [
+				'label' => $label,
+				'value' => $item['data']['value'],
+				'inlineStyles' => $styles,
+				'source' => $item['data']['source'] ?? "",
+				'item-name' => $item['data']['item-name']
+			];
+
+			return $result;
+		}, [ 'data' => [], 'renderLabels' => false ] );
+	}
+
+    private function createHorizontalGroupData( array $groupData ) {
+		$horizontalGroupData = [
+			'data' => [],
+			'renderLabels' => false
+		];
+
+		foreach ( $groupData as $item ) {
+			$data = $item['data'];
+
+			if ( $item['type'] === 'data' ) {
+				$horizontalGroupData['data'][] = [
+					'label' => $data['label'],
+					'value' => $data['value'],
+					'source' => $item['data']['source'] ?? "",
+					'item-name' => $item['data']['item-name']
+				];
+
+				if ( !empty( $data['label'] ) ) {
+					$horizontalGroupData['renderLabels'] = true;
+				}
+			} elseif ( $item['type'] === 'header' ) {
+				$horizontalGroupData['header'] = $data['value'];
+				// $horizontalGroupData['inlineStyles'] = $this->inlineStyles;
+			}
+		}
+
+		return $horizontalGroupData;
+	}
+
 }
