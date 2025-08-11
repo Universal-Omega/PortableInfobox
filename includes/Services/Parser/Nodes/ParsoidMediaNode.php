32b9fb97e8bb19c48cfe25cf4e69820cc9b53a3c
OriginalAuthority <luke@telepedia.net>
2025-08-12 00:02:54 +0100

    New nodes

--- includes/Services/Parser/Nodes/ParsoidMediaNode.php
+++ includes/Services/Parser/Nodes/ParsoidMediaNode.php
@@ -2,11 +2,19 @@
 
 namespace PortableInfobox\Services\Parser\Nodes;
 
+use MediaWiki\MediaWikiServices;
+use MediaWiki\Title\Title;
+use PortableInfobox\Parsoid\ParsoidPortableInfoboxRenderService;
+use PortableInfobox\Services\Helpers\FileNamespaceSanitizeHelper;
 use PortableInfobox\Services\Helpers\PortableInfoboxImagesHelper;
 
 class ParsoidMediaNode extends Node {
 
     private ?PortableInfoboxImagesHelper $helper;
+
+    private const ALLOWIMAGE_ATTR_NAME = 'image';
+	private const ALLOWVIDEO_ATTR_NAME = 'video';
+	private const ALLOWAUDIO_ATTR_NAME = 'audio';
 
     /**
      * Return the data for the image
@@ -63,10 +71,82 @@ class ParsoidMediaNode extends Node {
 		$helper = $this->getImageHelper();
 		$data = [];
         $parser = $this->getExternalParser();
-        $images = $parser->extractGallery( $value );
-		
-        // no-op right now
-        return [];
+        $items = $parser->extractGallery( $value );
+		// @TODO: do tabbers also 
+        foreach ( $items as $item ) {
+			$mediaItem = $this->getImageData( $item['title'], $item['label'], $item['label'] );
+			if ( (bool)$mediaItem ) {
+				$data[] = $mediaItem;
+			}
+		}
+
+        return count( $data ) > 1 ? $helper->extendImageCollectionData( $data ) : $data;
+	}
+
+    private function getImageData( $title, $alt, $caption ) {
+		$helper = $this->getImageHelper();
+		$titleObj = $title instanceof Title ? $title : $this->getImageAsTitleObject( $title );
+		$fileObj = $helper->getFile( $titleObj );
+
+		if ( !isset( $fileObj ) || !$this->isTypeAllowed( $fileObj->getMediaType() ) ) {
+			return [];
+		}
+
+		$mediatype = $fileObj->getMediaType();
+		$image = [
+			'url' => $this->resolveImageUrl( $fileObj, $titleObj ),
+			'name' => $titleObj ? $titleObj->getText() : '',
+			'alt' => $alt ?? ( $titleObj ? $titleObj->getText() : null ),
+			'caption' => $caption ?: null,
+			'isImage' => in_array( $mediatype, [ MEDIATYPE_BITMAP, MEDIATYPE_DRAWING ] ),
+			'isVideo' => $mediatype === MEDIATYPE_VIDEO,
+			'isAudio' => $mediatype === MEDIATYPE_AUDIO,
+			'source' => $this->getPrimarySource(),
+			'item-name' => $this->getItemName(),
+			'htmlAfter' => null,
+		];
+
+		if ( $image['isImage'] ) {
+			$image = array_merge( $image, $helper->extendImageData(
+				$fileObj,
+				ParsoidPortableInfoboxRenderService::DEFAULT_DESKTOP_THUMBNAIL_WIDTH,
+				ParsoidPortableInfoboxRenderService::DEFAULT_DESKTOP_INFOBOX_WIDTH
+			) );
+		}
+
+		return $image;
+	}
+
+    /**
+     * Get the image as a title object
+     * @param $imageName the image name 
+     * @return Title
+     */
+    private function getImageAsTitleObject( $imageName ): Title {
+		$contLang = MediaWikiServices::getInstance()->getContentLanguage();
+
+		$title = Title::makeTitleSafe(
+			NS_FILE,
+			FileNamespaceSanitizeHelper::getInstance()->sanitizeImageFileName( $imageName, $contLang )
+		);
+
+		return $title;
+	}
+
+    /**
+	 * Returns image url for given image title
+	 * @param File|null $file
+	 * @param Title|null $title
+	 * @return string url or '' if image doesn't exist
+	 */
+	public function resolveImageUrl( $file, $title ) {
+		global $wgPortableInfoboxUseFileDescriptionPage;
+
+		if ( $wgPortableInfoboxUseFileDescriptionPage && $title ) {
+			return $title->getLocalURL();
+		}
+
+		return $file ? $file->getUrl() : '';
 	}
 
     /**
@@ -78,5 +158,65 @@ class ParsoidMediaNode extends Node {
 			$this->helper = new PortableInfoboxImagesHelper();
 		}
 		return $this->helper;
+	}
+
+    /**
+	 * Checks if file media type is allowed
+	 * @param string $type
+	 * @return bool
+	 */
+	private function isTypeAllowed( $type ) {
+		switch ( $type ) {
+			case MEDIATYPE_BITMAP:
+			case MEDIATYPE_DRAWING:
+				return $this->allowImage();
+			case MEDIATYPE_VIDEO:
+				return $this->allowVideo();
+			case MEDIATYPE_AUDIO:
+				return $this->allowAudio();
+			default:
+				return false;
+		}
+	}
+
+    /**
+	 * @return bool
+	 */
+	protected function allowImage() {
+		$attr = $this->getXmlAttribute( $this->xmlNode, self::ALLOWIMAGE_ATTR_NAME );
+
+		return !( isset( $attr ) && strtolower( $attr ) === 'false' );
+	}
+
+	/**
+	 * @return bool
+	 */
+	protected function allowVideo() {
+		$attr = $this->getXmlAttribute( $this->xmlNode, self::ALLOWVIDEO_ATTR_NAME );
+
+		return !( isset( $attr ) && strtolower( $attr ) === 'false' );
+	}
+
+	/*
+	 * @return bool
+	 */
+	protected function allowAudio() {
+		$attr = $this->getXmlAttribute( $this->xmlNode, self::ALLOWAUDIO_ATTR_NAME );
+
+		return !( isset( $attr ) && strtolower( $attr ) === 'false' );
+	}
+
+    public function isEmpty() {
+		$data = $this->getData();
+		foreach ( $data as $dataItem ) {
+			if ( !empty( $dataItem['url'] ) ) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public function getType() {
+		return 'media';
 	}
 }
