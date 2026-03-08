434252381517a2f109cc05a7cf76c6001a0b8e1b
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-03-08 13:25:56 -0600

    Replace deprecated methods for Phan (hopefully correctly)

--- includes/Parsoid/InfoboxTag.php
+++ includes/Parsoid/InfoboxTag.php
@@ -2,6 +2,7 @@
 
 namespace PortableInfobox\Parsoid;
 
+use Wikimedia\Parsoid\Core\ContentMetadataCollectorStringSets as CMCSS;
 use Wikimedia\Parsoid\Ext\ExtensionModule;
 use Wikimedia\Parsoid\Ext\ExtensionTagHandler;
 use Wikimedia\Parsoid\Ext\ParsoidExtensionAPI;
@@ -17,12 +18,12 @@ class InfoboxTag extends ExtensionTagHandler implements ExtensionModule {
 			'tags' => [
 				[
 					'name' => 'infobox',
-					'handler' => self::class
-				]
+					'handler' => self::class,
+				],
 			],
 			'domProcessors' => [
-				'PortableInfobox\\Parsoid\\PortableInfoboxDOMProcessor'
-			]
+				'PortableInfobox\\Parsoid\\PortableInfoboxDOMProcessor',
+			],
 		];
 	}
 
@@ -34,16 +35,12 @@ class InfoboxTag extends ExtensionTagHandler implements ExtensionModule {
 			'wrapperTag' => 'aside',
 			'parseOpts' => [
 				'extTag' => 'infobox',
-				'context' => 'inline'
-			]
+				'context' => 'inline',
+			],
 		] );
 
-		// this is a bit messed up as these methods are deprecated, but the documentation
-		// for the replacement methods doesn't exist or make sense
-		// this is commented out at present, as these scripts and styles will be added
-		// by the legacy parser (might need it here to stick these in the parser cache also?)
-		$api->getMetadata()->addModules( [ 'ext.PortableInfobox.scripts' ] );
-		$api->getMetadata()->addModuleStyles( [ 'ext.PortableInfobox.styles' ] );
+		$api->getMetadata()->appendOutputStrings( CMCSS::MODULE_STYLE, [ 'ext.PortableInfobox.styles' ] );
+		$api->getMetadata()->appendOutputStrings( CMCSS::MODULE, [ 'ext.PortableInfobox.scripts' ] );
 
 		// return this back. At this point, we have constructed the outer tag (<aside class=...</aside>)
 		// and this function is done with its work. The rest of the work will happen in the DOMProcessor
